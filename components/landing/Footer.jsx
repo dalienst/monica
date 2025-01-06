2d57b0e069d49f605763efb2a6fcd069b5a67e7b
dalienst <oduordalienst@gmail.com>
2025-01-06 20:33:16 +0300

    add contact

--- components/landing/Footer.jsx
+++ components/landing/Footer.jsx
@@ -1,11 +1,12 @@
+import { contacts } from "@/data/contact";
 import Link from "next/link";
 import React from "react";
 
 function Footer() {
   return (
-    <div className="container-fluid footer py-3">
+    <div className="footer py-5">
       <section className="container">
-        <div className="row">
+        <div className="row mb-3">
           <div className="col-md-4 col-sm-12 mb-3">
             <h4>Monica Tours & Safari</h4>
             <p>
@@ -41,24 +42,20 @@ function Footer() {
             <h4>Contact</h4>
             <hr className="text-white" />
             <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <Link href="/" className="nav-link p-0 text-white">
-                  <i className="bi bi-envelope"></i> Email
-                </Link>
-              </li>
-              <li className="nav-item mb-2">
-                <Link href="/" className="nav-link p-0 text-white">
-                  <i className="bi bi-telephone"></i> Phone
-                </Link>
-              </li>
-              <li className="nav-item mb-2">
-                <Link href="/" className="nav-link p-0 text-white">
-                  <i className="bi bi-instagram"></i> Instagram
-                </Link>
-              </li>
+              {contacts?.map((contact) => (
+                <li key={contact?.id} className="nav-item mb-2">
+                  <Link
+                    href={contact?.link}
+                    className="nav-link p-0 text-white"
+                  >
+                    {contact?.icon} {contact?.title}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
+        <p className="text-light small">Copyright &copy; 2025 Monica Tours & Safaris</p>
       </section>
     </div>
   );
--- data/contact.jsx
+++ data/contact.jsx
@@ -23,4 +23,37 @@ const faq = [
   },
 ];
 
-export { faq };
+const contacts = [
+  {
+    id: 1,
+    title: "monicasafaris@gmail.com",
+    link: "mailto:monicasafaris@gmail.com",
+    icon: <i className="bi bi-envelope"></i>,
+  },
+  {
+    id: 2,
+    title: "+1 (123) 456-7890",
+    link: "tel:+1 (123) 456-7890",
+    icon: <i className="bi bi-telephone"></i>,
+  },
+  {
+    id: 3,
+    title: "123 Main Street, City, Country",
+    link: "https://www.google.com/maps",
+    icon: <i className="bi bi-geo-alt"></i>,
+  },
+  {
+    id: 4,
+    title: "@monicasafaris",
+    link: "https://twitter.com/monicasafaris",
+    icon: <i className="bi bi-twitter"></i>,
+  },
+  {
+    id: 5,
+    title: "@monicasafaris",
+    link: "https://www.instagram.com/monicasafaris",
+    icon: <i className="bi bi-instagram"></i>,
+  },
+];
+
+export { faq, contacts };
