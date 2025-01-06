81eaa19dede184c032909572b0ffbc355a73e867
dalienst <oduordalienst@gmail.com>
2025-01-06 13:48:54 +0300

    added image

--- components/landing/Header.jsx
+++ components/landing/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="container-fluid header-bg mb-3 d-flex flex-column align-items-center justify-content-center text-center mx-auto">
+      <div>
+        <h1>Explore the Wild Like Never Before!</h1>
+      </div>
+
+      <div>
+        <h6>Tailored tours for unforgettable adventures.</h6>
+      </div>
+
+      <div>
+        <Link href="/" className="btn btn-primary me-3">
+          Explore Tours
+        </Link>
+        <Link href="/" className="btn btn-secondary">
+          Book A Trip
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
