18bccc13bb84bdae38d94f8bd8cf0b9bcd173800
dalienst <oduordalienst@gmail.com>
2025-01-06 15:43:51 +0300

    add excursions section

--- components/landing/Excursions.jsx
+++ components/landing/Excursions.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Excursions() {
   return (
-    <div>Excursions</div>
-  )
+    <section className="bg-white py-5" id="top-excursions">
+      <div className="text-center">
+        <h5 className="fw-bold text-uppercase about-heading">
+          Excursions & Safaris
+        </h5>
+        <h2>Our Top Excursions</h2>
+        <hr className="about-hr w-25 m-auto border-5" />
+      </div>
+    </section>
+  );
 }
 
-export default Excursions
+export default Excursions;
---
+++ data/excursions.jsx
@@ -0,0 +1,52 @@
+const topExcursions = [
+  {
+    id: 1,
+    title: "1 Day Tsavo East",
+    image: "./tsavo.jpg",
+    pound: "£ 180",
+    dollar: "$ 220",
+    euro: "€ 170",
+  },
+  {
+    id: 2,
+    title: "1 Day Tsavo East",
+    image: "./tsavo.jpg",
+    pound: "£ 180",
+    dollar: "$ 220",
+    euro: "€ 170",
+  },
+  {
+    id: 3,
+    title: "1 Day Tsavo East",
+    image: "./tsavo.jpg",
+    pound: "£ 180",
+    dollar: "$ 220",
+    euro: "€ 170",
+  },
+  {
+    id: 4,
+    title: "1 Day Tsavo East",
+    image: "./tsavo.jpg",
+    pound: "£ 180",
+    dollar: "$ 220",
+    euro: "€ 170",
+  },
+  {
+    id: 5,
+    title: "1 Day Tsavo East",
+    image: "./tsavo.jpg",
+    pound: "£ 180",
+    dollar: "$ 220",
+    euro: "€ 170",
+  },
+  {
+    id: 6,
+    title: "1 Day Tsavo East",
+    image: "./tsavo.jpg",
+    pound: "£ 180",
+    dollar: "$ 220",
+    euro: "€ 170",
+  },
+];
+
+export { topExcursions };
