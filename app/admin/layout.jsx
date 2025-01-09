c3c86a372b7eac2b8bb5d4b197d98d3e64413c37
dalienst <oduordalienst@gmail.com>
2025-01-09 21:06:09 +0300

    added navbar

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,8 +1,14 @@
 "use client";
+import Navbar from "@/components/admin/Navbar";
 import React from "react";
 
 function AdminLayout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
 
 export default AdminLayout;
---
+++ components/admin/Navbar.jsx
@@ -0,0 +1,56 @@
+"use client";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function Navbar() {
+  const pathname = usePathname();
+  return (
+    <nav className="navbar navbar-expand-md sticky-top border-bottom bg-white">
+      <div className="container-fluid">
+        <Link href="/" className="navbar-brand">
+          Admin Panel
+        </Link>
+
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavbar"
+          aria-controls="collapsibleNavbar"
+        >
+          <i className="bi bi-list text-dark"></i>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="collapsibleNavbar"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link href="/admin/dashboard" className="nav-link">
+                Dashboard
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link href="/admin/dashboard" className="nav-link">
+                Tours
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <button
+                onClick={() => signOut()}
+                className="nav-link text-danger"
+              >
+                Sign Out
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
