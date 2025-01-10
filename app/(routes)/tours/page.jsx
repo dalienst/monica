350c6fdbf378cb45340b52ee997caffa6e2b665e
dalienst <oduordalienst@gmail.com>
2025-01-10 17:46:01 +0300

    display tours

--- app/(routes)/tours/page.jsx
+++ app/(routes)/tours/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+import LoadingSpinner from "@/components/general/LoadingSpinner";
+import Header from "@/components/tours/Header";
+import TourCard from "@/components/tours/TourCard";
+import { useFetchAllTours } from "@/hooks/tours/actions";
+import React from "react";
 
 function Tours() {
+  const {
+    isLoading: isLoadingTours,
+    data: tours,
+    refetch: refetchTours,
+  } = useFetchAllTours();
+
+  if (isLoadingTours) return <LoadingSpinner />;
+
   return (
-    <div>Tours</div>
-  )
+    <>
+      <Header />
+      <div className="container py-5">
+        <div className="row mb-3">
+          {tours?.map((tour) => (
+            <TourCard key={tour?.id} tour={tour} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Tours
+export default Tours;
---
+++ components/tours/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="container-fluid tour-bg d-flex flex-column align-items-center justify-content-center text-center mx-auto"></div>
+  )
+}
+
+export default Header
