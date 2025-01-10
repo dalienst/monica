051956ef1c78b78b80f871e33cd1f8ad02a86820
dalienst <oduordalienst@gmail.com>
2025-01-10 21:52:58 +0300

    modified tour detail page

--- app/(routes)/tours/[tourReference]/page.jsx
+++ app/(routes)/tours/[tourReference]/page.jsx
@@ -28,32 +28,38 @@ function TourDetail({ params }) {
       />
       <div className="container py-3 pb-5">
         <div className="row">
+          {/* Tour Details Section */}
           <div className="col-md-8 col-sm-12 mb-3">
-            <section className="mt-3 mb-3">
-              <h2>{tour?.title}</h2>
-            </section>
+            <div className="card h-100 p-4">
+              <section className="mb-3">
+                <h2 className="mb-3 text-primary">{tour?.title}</h2>
+              </section>
 
-            <section className="mb-3">
-              <h6>Rates</h6>
-              <p>
-                ${tour?.dollar} | €{tour?.euro} | £{tour?.pound}
-              </p>
-            </section>
+              <section className="mb-3">
+                <h6 className="text-secondary">Rates</h6>
+                <p className="fs-5">
+                  <strong>${tour?.dollar}</strong> | €{tour?.euro} | £
+                  {tour?.pound}
+                </p>
+              </section>
 
-            <section className="mb-3">
-              <h6>Tour Details</h6>
-              <p>
-                <i className="bi bi-geo-alt" /> {tour?.duration} days
-              </p>
-              <p>
-                <i className="bi bi-people" /> {tour?.capacity} people
-              </p>
-            </section>
+              <section className="mb-3">
+                <h6 className="text-secondary">Tour Details</h6>
+                <p>
+                  <i className="bi bi-geo-alt text-primary" /> {tour?.duration}{" "}
+                  days
+                </p>
+                <p>
+                  <i className="bi bi-people text-primary" /> {tour?.capacity}{" "}
+                  people
+                </p>
+              </section>
 
-            <section className="mb-3">
-              <h6>Description</h6>
-              <p>{tour?.description}</p>
-            </section>
+              <section className="mb-3">
+                <h6 className="text-secondary">Description</h6>
+                <p>{tour?.description}</p>
+              </section>
+            </div>
           </div>
 
           <div className="col-md-4 col-sm-12 mb-3">
