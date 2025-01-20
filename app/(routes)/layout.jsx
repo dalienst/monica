47e06a5810e450ce54bf446ea0a93e04b4affd7d
dalienst <oduordalienst@gmail.com>
2025-01-20 19:00:09 +0300

    add whatsapp

--- app/(routes)/layout.jsx
+++ app/(routes)/layout.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Footer from "@/components/landing/Footer";
 import Navbar from "@/components/landing/Navbar";
+import Link from "next/link";
 import React from "react";
 
 function BaseLayout({ children }) {
@@ -9,6 +10,15 @@ function BaseLayout({ children }) {
       <Navbar />
       {children}
       <Footer />
+
+      <Link
+        href="https://wa.me/254788089205"
+        className="whatsapp-button"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="bi bi-whatsapp"></i>
+      </Link>
     </div>
   );
 }
